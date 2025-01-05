e201f31ed1be80f9bd67fa4683e35ba49d3bf4f9
Parsa Tajik <parsa1379.tajik@gmail.com>
2025-01-04 20:29:05 -0800

    updates

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCoWwl1DxQa3qoX-PmvQe5sqiybzkXYNug",
-  authDomain: "artefice-bloorsoft.firebaseapp.com",
-  projectId: "artefice-bloorsoft",
-  storageBucket: "artefice-bloorsoft.appspot.com",
-  messagingSenderId: "244209947040",
-  appId: "1:244209947040:web:247853eaaecc4450e894c5",
-  measurementId: "G-89KSWYHQ53",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
